a1adb94c6ec93b5feed6a8b24901566bf03200d8
Marius Schmeling <schmeling@vr.rwth-aachen.de>
2019-05-23 14:42:28 +0200

    deleted merge errors 2

--- Source/VAPlugin/Private/VASourceComponent.cpp
+++ Source/VAPlugin/Private/VASourceComponent.cpp
@@ -124,14 +124,8 @@ void UVASourceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 		// update Pos
 		if (vMovement == EMovement::MoveWithObject) {
 
-
 			FTransform trans = ownerActor->GetTransform();
-			FVector loc = trans.GetLocation();
-			FRotator rot = trans.GetRotation().Rotator();
 
-			loc = loc + vOffset;
-
-			FVAPluginModule::updateSourcePos(soundID, loc, rot);
 			FVector pos = trans.GetLocation();
 			FRotator rot = trans.GetRotation().Rotator();
 
