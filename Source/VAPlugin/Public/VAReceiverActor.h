eb3079785f58b8157bd1bc0f81a8ef2ffe55a9dd
Marius Schmeling <schmeling@vr.rwth-aachen.de>
2019-09-26 15:23:13 +0200

    commented out endplay from receiver actor 2

--- Source/VAPlugin/Public/VAReceiverActor.h
+++ Source/VAPlugin/Public/VAReceiverActor.h
@@ -71,8 +71,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// Called when the game starts or when spawned
-	virtual void EndPlay();
+	// // Called when the game starts or when spawned
+	// virtual void EndPlay();
 	
 	// Updates the Virtual World Position of the Player // 
 	bool updateVirtualWorldPosition();
