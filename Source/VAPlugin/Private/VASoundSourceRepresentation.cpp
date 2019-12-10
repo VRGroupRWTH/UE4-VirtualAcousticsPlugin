f0e69a52384dc4fb668741a5e3b042cdd12fb8b1
jwendt <wendt@vr.rwth-aachen.de>
2019-12-10 12:12:16 +0100

    test out other way to find debug cone

--- Source/VAPlugin/Private/VASoundSourceRepresentation.cpp
+++ Source/VAPlugin/Private/VASoundSourceRepresentation.cpp
@@ -20,7 +20,7 @@ AVASoundSourceRepresentation::AVASoundSourceRepresentation()
 	RootComponent = sphereComp;
 	
 	sphereMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMeshAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cone.Shape_Cone"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMeshAsset(TEXT("/VAPlugin/Shape_Cone"));
 	if (SphereMeshAsset.Succeeded()) {
 		sphereMesh->SetStaticMesh(SphereMeshAsset.Object);
 		// sphereMesh->AttachTo(RootComponent);
