b13cb8c3c4c2b480c89d787f0194a6c709e136f5
Philipp Schäfer <philipp.schaefer@akustik.rwth-aachen.de>
2021-04-14 16:14:59 +0200

    Untroducing controller which is able to set global and renderer auralization mode

---
+++ Source/VAPlugin/Private/AuralizationMode/VAAuralizationModeController.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuralizationMode/VAAuralizationModeController.h"
+
+
+#include "VAUtils.h"
+#include "VAPlugin.h"
+
+// Sets default values for this component's properties
+UVAAuralizationModeController::UVAAuralizationModeController()
+	: Target(Global)
+{
+}
+
+
+UVAAuralizationModeController::UVAAuralizationModeController(const std::string& RendererName)
+	: Target(Renderer)
+	, RendererName(RendererName)
+{
+}
+
+
+void UVAAuralizationModeController::Initialize()
+{
+	if (bInitialized)
+	{
+		return;
+	}
+
+	AuralizationModeVector.set();
+	AuralizationModeVector[(int)EAuralizationMode::DirectSound] = bEnableDirectSound;
+	AuralizationModeVector[(int)EAuralizationMode::EarlyReflections] = bEnableEarlyReflections;
+	AuralizationModeVector[(int)EAuralizationMode::DiffuseDecay] = bEnableDiffuseDecay;
+	AuralizationModeVector[(int)EAuralizationMode::SourceDirectivity] = bEnableSourceDirectivity;
+	AuralizationModeVector[(int)EAuralizationMode::MediumAbsorption] = bEnableMediumAbsorption;
+	AuralizationModeVector[(int)EAuralizationMode::TemporalVariation] = bEnableTemporalVariations;
+	AuralizationModeVector[(int)EAuralizationMode::Doppler] = bEnableDoppler;
+	SendAuralizationModeToVA(AuralizationModeVector);
+	bInitialized = true;
+}
+
+
+bool UVAAuralizationModeController::SetAuralizationMode(EAuralizationMode::Type AuralizationMode, bool bEnable)
+{
+	std::bitset<NumAuralizationModes> TmpAuraModeBitvector = AuralizationModeVector;
+	TmpAuraModeBitvector[int(AuralizationMode)] = bEnable;
+
+	return SendAuralizationModeToVA(TmpAuraModeBitvector);
+}
+
+bool UVAAuralizationModeController::ToggleAuralizationMode(EAuralizationMode::Type AuralizationMode)
+{
+	std::bitset<NumAuralizationModes> TmpAuraModeBitvector = AuralizationModeVector;
+	TmpAuraModeBitvector.flip(int(AuralizationMode));
+
+	return SendAuralizationModeToVA(TmpAuraModeBitvector);
+}
+
+bool UVAAuralizationModeController::GetAuralizationMode(EAuralizationMode::Type AuralizationMode) const
+{
+	return AuralizationModeVector[AuralizationMode];
+}
+
+bool UVAAuralizationModeController::SendAuralizationModeToVA(const AuralizationBitvector& AuraModeBitvector)
+{
+	bool ok = false;
+	switch (Target)
+	{
+	case Global:
+		ok = FVAPlugin::SetGlobalAuralizationMode((int)AuraModeBitvector.to_ulong());
+		break;
+	case Renderer:
+		ok = FVAPlugin::SetRenderingModuleAuralizationMode(RendererName, (int)AuraModeBitvector.to_ulong());
+		break;
+	}
+	if (ok)
+	{
+		AuralizationModeVector = AuraModeBitvector;
+	}
+	return ok;
+}
+
---
+++ Source/VAPlugin/Private/AuralizationMode/VAAuralizationModeController.h
@@ -0,0 +1,124 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+
+#include <string>
+#include <bitset>
+
+
+#include "VAAuralizationModeController.generated.h"
+
+
+UENUM(BlueprintType)
+namespace EAuralizationMode
+{
+	enum Type
+	{
+		DirectSound = 0,
+		EarlyReflections = 1,
+		DiffuseDecay = 2,
+		SourceDirectivity = 3,
+		MediumAbsorption = 4,
+		TemporalVariation = 5,
+		//Scattering = 6,
+		//Diffraction = 7,
+		//NearField = 8,
+		Doppler = 9
+		//SpreadingLoss = 10,
+		//Transmission = 11,
+		//Absorption = 12
+	};
+}
+
+
+UCLASS(ClassGroup = (VA), AutoExpandCategories = ("General", "Obstacle Interaction", "Sound Source Related", "Medium Interaction", "Dynamic Effects"),
+	meta = (Tooltip = "Only attach to VAReceiverActor, VARenderer (future release) or VASourceComponent"))
+class UVAAuralizationModeController : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// Create a controller for global auralization mode
+	UVAAuralizationModeController();
+	// Create a controller for renderer auralization mode
+	UVAAuralizationModeController(const std::string& RendererName);
+
+	// initialize Sound Source with the settings set // 
+	void Initialize();
+
+public:
+
+	// Sets the specified auralization mode to given value (true/false)
+	UFUNCTION(BlueprintCallable)
+	bool SetAuralizationMode(EAuralizationMode::Type AuralizationMode, bool bEnable);
+
+	// Toggles the specified auralization mode
+	UFUNCTION(BlueprintCallable)
+	bool ToggleAuralizationMode(EAuralizationMode::Type AuralizationMode);
+
+	// Returns the state of given auralization mode
+	UFUNCTION(BlueprintCallable)
+	bool GetAuralizationMode(EAuralizationMode::Type AuralizationMode) const;
+
+protected:
+	const static int NumAuralizationModes = 13;
+	typedef std::bitset<NumAuralizationModes> AuralizationBitvector;
+
+	// Sends the given auralization mode bit vector to VA and updates local bitvector on on success
+	// @return true on success
+	bool SendAuralizationModeToVA(const AuralizationBitvector& AuraModeBitvector);
+
+
+
+	// Toggle direct sound
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Direct sound (DS)", Category = "General"))
+		bool bEnableDirectSound = true;
+
+	// Toggle early reflections
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Early reflections (ER)", Category = "Obstacle Interaction"))
+		bool bEnableEarlyReflections = true;
+
+	// Toggle diffuse decay
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Diffuse decay (DD)", Category = "Obstacle Interaction"))
+		bool bEnableDiffuseDecay = true;
+
+	// Toggle source directivity
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Source directivity (SD)", Category = "Sound Source Related"))
+		bool bEnableSourceDirectivity = true;
+
+	// Toggle medium absorption
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Medium absorption (MA)", Category = "Medium Interaction"))
+		bool bEnableMediumAbsorption = true;
+
+	// Toggle temporal variations
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Temporal variations (TV)", Category = "Medium Interaction"))
+		bool bEnableTemporalVariations = true;
+
+	//MISSING: Scattering (SC), Diffraction (DF), Near field (NF)
+
+	// Toggle doppler effect
+	UPROPERTY(EditAnywhere, meta = (DisplayName = "Doppler effect (DP)", Category = "Dynamic Effects"))
+		bool bEnableDoppler = true;
+
+	//MISSING: Spreading loss (SL), Transmission (TR), Absorption (AB)
+
+	enum ETarget
+	{
+		Global = 0,
+		Renderer = 1
+		//SoundSource = 2 //For future
+	};
+	ETarget Target;
+
+	// Name used to address renderer
+	std::string RendererName;
+
+	// Stores the bitvector containing all auralization modes
+	AuralizationBitvector AuralizationModeVector;
+
+	bool bInitialized = false;
+};
