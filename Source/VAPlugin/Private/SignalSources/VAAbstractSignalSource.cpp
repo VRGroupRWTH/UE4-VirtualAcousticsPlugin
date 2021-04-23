7b62996b760e60cae6d5c2c485271de074056c81
Ehret <jw210150@WIN.RZ.RWTH-AACHEN.DE>
2021-04-23 13:03:48 +0200

    use paths for now public include

--- Source/VAPlugin/Private/SignalSources/VAAbstractSignalSource.cpp
+++ Source/VAPlugin/Private/SignalSources/VAAbstractSignalSource.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "VAAbstractSignalSource.h"
+#include "SignalSources/VAAbstractSignalSource.h"
 
 #include "VADefines.h"
 
