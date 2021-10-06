a194c594cada884fb0877d49a55ecbaad86243ed
Philipp Schäfer <pschaefer@ITA.AKUSTIK.RWTH-AACHEN.DE>
2021-10-06 13:42:33 +0200

    VADirectPlaybackPseudoRenderer: changed directory of IR wav files

--- Source/VAPlugin/Private/Renderer/VADirectPlaybackPseudoRenderer.cpp
+++ Source/VAPlugin/Private/Renderer/VADirectPlaybackPseudoRenderer.cpp
@@ -7,20 +7,22 @@
 #include "SoundSource/VAAbstractSourceComponent.h"
 
 
+#define DIRAC_IR_WAV "direct_playback_renderer/DiracIR.wav"
+#define NULL_IR_WAV "direct_playback_renderer/NullIR.wav"
 
 void AVADirectPlaybackPseudoRenderer::ActivateChannel(const AVAReceiverActor* VAReceiver, const UVAAbstractSourceComponent* VASource, int ChannelID) const
 {
-	Super::SetChannelIR(VAReceiver, VASource, "DiracIR.wav", ChannelID);
+	Super::SetChannelIR(VAReceiver, VASource, DIRAC_IR_WAV, ChannelID);
 }
 
 void AVADirectPlaybackPseudoRenderer::DeactivateChannel(const AVAReceiverActor* VAReceiver, const UVAAbstractSourceComponent* VASource, int ChannelID) const
 {
-	Super::SetChannelIR(VAReceiver, VASource, "NullIR.wav", ChannelID);
+	Super::SetChannelIR(VAReceiver, VASource, NULL_IR_WAV, ChannelID);
 }
 
 void AVADirectPlaybackPseudoRenderer::SetSingleChannelActive(const AVAReceiverActor* VAReceiver, const UVAAbstractSourceComponent* VASource, int ChannelID) const
 {
-	Super::SetAllIRs(VAReceiver, VASource, "NullIR.wav");
+	Super::SetAllIRs(VAReceiver, VASource, NULL_IR_WAV);
 	ActivateChannel(VAReceiver, VASource, ChannelID);
 }
 
