2cf0173420178fc008b3ade832ca24b397b99bdf
Ehret <jw210150@WIN.RZ.RWTH-AACHEN.DE>
2021-11-24 17:32:09 +0100

    fix fix ;-)

--- Source/VAPlugin/Private/Renderer/VABaseRenderer.cpp
+++ Source/VAPlugin/Private/Renderer/VABaseRenderer.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Renderer/VABaseRenderer.h"
+
 #include "Utility/VirtualRealityUtilities.h"
 
-#include "Renderer/VABaseRenderer.h"
 
 #include "VAPlugin.h"
 #include "VAUtils.h"
